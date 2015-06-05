0750d2b6888c6d5482a402b9ef1725e82dff2293
Toni Menzel <toni.menzel@rebaze.com>
2015-06-05 15:07:44 +0200

    reduced overlap between extension (collecting payload) and plugin (explicit deployment)

--- src/main/java/com/rebaze/maven/payload/PayloadEventSpy.java
+++ src/main/java/com/rebaze/maven/payload/PayloadEventSpy.java
@@ -10,57 +10,33 @@
  *******************************************************************************/
 package com.rebaze.maven.payload;
 
+import org.apache.maven.MavenExecutionException;
 import org.apache.maven.eventspy.AbstractEventSpy;
-import org.apache.maven.execution.DefaultMavenExecutionResult;
 import org.apache.maven.execution.ExecutionEvent;
-import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.settings.building.SettingsBuildingRequest;
-import org.apache.maven.settings.building.SettingsBuildingResult;
-import org.codehaus.plexus.logging.Logger;
 import org.eclipse.aether.RepositoryEvent;
-import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.RepositorySystemSession;
-import org.eclipse.aether.repository.RemoteRepository;
 
-import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 /**
+ * A dependency tracing spy.
+ * Dumps all relevant repository interaction to a file so we can provision
+ * repositories with dependencies.
+ *
  * @author Toni Menzel (toni.menzel@rebaze.com)
- *         <p/>
- *         A dependency tracing spy.
- *         Dumps all relevant repository interaction to a file so we can provision
- *         repositories with dependencies.
- *         <p/>
- *         TODO: factor out upload to mojo.
  */
 @Named
 public class PayloadEventSpy extends AbstractEventSpy
 {
-    private static final String PAYLOAD_FILENAME = "build.payload";
-
-    /**
-     * When enabled, this spy will trace dependencies requested during builds in file target/PAYLOAD_FILENAME.
-     */
-    public static String PROPERTY_ENABLED = "payload.enabled";
-
-    @Inject
-    private Logger logger;
-
-    @Inject RepositorySystem repoSystem;
-
-    private MavenProject m_reactorProject;
+    public static final String PAYLOAD_FILENAME = "build.payload";
 
     private List<RepositoryEvent> m_eventLog = new ArrayList<>();
-
-    private MavenExecutionRequest execRequest;
-
-    private boolean enabled = false;
-
-    private List<String> m_payloadGavs;
+    private MavenProject m_reactorProject;
 
     @Override public void onEvent( Object event ) throws Exception
     {
@@ -72,11 +48,10 @@ public class PayloadEventSpy extends AbstractEventSpy
                 org.apache.maven.execution.ExecutionEvent exec = ( ExecutionEvent ) event;
                 if ( exec.getProject() != null && exec.getProject().isExecutionRoot() )
                 {
-                    if ( m_reactorProject == null )
+                    if (m_reactorProject == null)
                     {
                         m_eventLog = new ArrayList<>();
                         m_reactorProject = exec.getProject();
-                        m_payloadGavs = readInputBill( getPayloadFile() );
                     }
                 }
             }
@@ -88,30 +63,24 @@ public class PayloadEventSpy extends AbstractEventSpy
         }
         catch ( Exception e )
         {
-            logger.error( "Problem!", e );
+            throw new MavenExecutionException( "Problem!", e );
         }
     }
 
     @Override public void close() throws Exception
     {
-        if ( enabled && m_reactorProject != null )
-        {
-            writeDependencyList( getPayloadFile(), synth( m_eventLog ) );
-        }
-        super.close();
+        writeDependencyList( getPayloadFile(), synth( m_eventLog ) );
     }
 
-    private File writeDependencyList( File f, List<String> sorted )
+    private File writeDependencyList( File f, List<String> sorted ) throws MavenExecutionException
     {
         if ( !f.getParentFile().exists() )
         {
             f.getParentFile().mkdirs();
         }
 
-        int finalSize = -1;
         try ( BufferedWriter writer = new BufferedWriter( new FileWriter( f, true ) ) )
         {
-            finalSize = sorted.size();
             for ( String s : sorted )
             {
                 writer.append( s );
@@ -120,10 +89,8 @@ public class PayloadEventSpy extends AbstractEventSpy
         }
         catch ( IOException e )
         {
-            logger.error( "Problem writing file.", e );
+            throw new MavenExecutionException( "Problem writing payload file to " + f.getAbsolutePath(), e );
         }
-        logger.info( "Halo written (count=" + finalSize + "): " + f.getAbsolutePath() );
-
         return f;
     }
 
@@ -145,30 +112,5 @@ public class PayloadEventSpy extends AbstractEventSpy
     private File getPayloadFile()
     {
         return new File( new File( m_reactorProject.getBuild().getOutputDirectory() ).getParent(), PAYLOAD_FILENAME );
-    }
-
-    private List<String> readInputBill( File input )
-    {
-        if ( input == null || !input.exists() )
-        {
-            return null;
-        }
-        List<String> sortedArtifacts = new ArrayList<>();
-        try ( BufferedReader reader = new BufferedReader( new FileReader( input ) ) )
-        {
-            String line = null;
-            logger.info( "Reading payload " + input.getAbsolutePath() );
-            while ( ( line = reader.readLine() ) != null )
-            {
-                sortedArtifacts.add( line );
-            }
-        }
-        catch ( IOException e )
-        {
-            logger.error( "Cannot parse payload: " + input.getAbsolutePath(), e );
-            return null;
-        }
-
-        return sortedArtifacts;
     }
 }
